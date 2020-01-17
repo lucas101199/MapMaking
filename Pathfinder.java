6d295a2f3bad06e80490e2ecc5659c5bd3a09785
Frodo <frowin.fleig@web.de>
2020-01-17 12:54:58 +0100

    changes in Pathfinder.java

--- Pathfinder.java
+++ Pathfinder.java
@@ -1,9 +1,40 @@
 
 public class Pathfinder {
 
+    enum Dir {
 
-    public void findPath(Point start, Point goal, double[][] map) {
-
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
     }
 
+    private Dir[][] heatMap;
+
+
+    public void findPath(Point start, Point goal, double[][] map) {
+        heatMap = initHeatMap(map);
+    }
+
+
+
+    private Dir[][] initHeatMap(double[][] map) {
+        return new Dir[map.length][map[0].length];
+    }
+
+    private int x2Grid(int xValue, int gridWidth) {
+        return (xValue / gridWidth);
+    }
+
+    private int y2Grid(int yValue, int gridHeight) {
+        return (yValue / gridHeight);
+    }
+
+    private int Grid2x(int column, int gridWidth) {
+        return (column * gridWidth + (gridWidth / 2));
+    }
+
+    private int Grid2y(int row, int gridHeight) {
+        return (row * gridHeight + (gridHeight / 2));
+    }
 }
